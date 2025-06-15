f115ee6552abe2bbfea26dc6f710a594b864c7aa
cooku222 <aftermatter0901@daum.net>
2025-06-15 16:30:58 +0900

    성공, client Key는 가림 + 공식문서 수정사항 확인하기(ncpKeyId)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
+import { NavermapsProvider } from "react-naver-maps";
+import NaverMapsMarkerCluster from "./components/NaverMapsMarkerCluster";
+ 
 function App() {
+  // ncpClientId에 네이버 지도 API 클라이언트 키를 넣으면 된다.
+  // npx create-react-app으로 프로젝트를 생성했다면-별도의 의존성 설치 없이-프로젝트 최상위 폴더에 .env 파일을 생성하고 키를 기입하면 된다.
+  // .env에는 REACT_APP_NAVER_KEY의 값으로 키를 기입하면 되는데, REACT_APP_라는 prefix에 유의 하자!
+  const naverKey = process.env.REACT_APP_NAVER_KEY;
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavermapsProvider
+      ncpClientId={naverKey} // 지도서비스 Client ID
+      error={<p>error</p>}
+      loading={<p>Maps Loading</p>}
+    >
+      <div style={{ display: "flex", width: "100dvw", height: "100dvh" }}>
+        <NaverMapsMarkerCluster />
+      </div>
+      
+    </NavermapsProvider>
   );
 }
-
+ 
 export default App;
